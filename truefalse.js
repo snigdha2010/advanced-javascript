fd8c3bf25d9621ab327253033be25fec3bd1a29d
Snigdha Sahara <snigdhasahara@Snigdhas-MBP.fios-router.home>
2020-03-03 16:42:34 -0500

    true vs false

---
+++ truefalse.js
@@ -0,0 +1,21 @@
+
+const name ="";
+if(name){
+    console.log("condition is true");
+}else {
+    console.log("condition is false");
+}
+
+const sname = " " ;
+if(sname){
+    console.log("condition is true");
+} else{
+    console.log("condition is false");
+}
+
+let lname = [];
+if(lname){
+    console.log("condition id true.");
+}else{
+    console.log("condition is false");
+}
