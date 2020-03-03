8adcfb3ae65194ef22eb544456dcb9d5c02bdc9c
Snigdha Sahara <snigdhasahara@Snigdhas-MBP.fios-router.home>
2020-03-03 17:19:12 -0500

    null vs undefined completed

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let pakhi;
+
+// //no return 
+// function add(a,b){
+//     a+b;
+// }
+// console.log(pakhi);
+
+//parameter not passes 
+function add(a,b){
+   console.log(a);
+}
+const result = add(23);
+console.log(result);
+
+//object property 
+const man = {name:"rima",age:23};
+console.log(man.city);
